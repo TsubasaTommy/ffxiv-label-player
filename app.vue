14d965ac74f2ae9297d14aaef8a86664d65bba30
TsubasaTommy <i.love.hard.player@gmail.com>
2022-11-04 16:28:16 +0900

    First launch

---
+++ app.vue
@@ -0,0 +1,196 @@
+<template>
+  <div>
+    <div v-show="toggle.firstpage" class="hero min-h-screen bg-base-200">
+      <div class="hero-content flex-col lg:flex-row">
+        <img src="./static/logos/logo.png" class="max-w-sm rounded-lg shadow-2xl" />
+        <div>
+          <h1 class="text-5xl font-bold">FFXIV Player Label</h1>
+          <p class="py-6">
+            ようこそ♥このプロジェクトはエゴイストのスタッフさんのために創設されました🎉<br>
+            自動でキャラクターを検知してキャラクターの情報を知らせてくれるウェブアプリです♥
+          </p>
+          <button @click="toggle.firstpage = false" class="btn btn-primary">Get Started</button>
+        </div>
+      </div>
+    </div>
+
+
+
+    <div v-show="!toggle.firstpage" class="hero min-h-screen bg-base-200">
+      <div class="hero-content flex-col lg:flex-row">
+        
+        
+        
+        <div class="overflow-y-auto">
+          <table class="table table-compact w-full">
+            <thead>
+              <tr>
+                <th></th>
+                <th>キャラクター名</th>
+                <th>ラベル</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(item, index) in players" :key="index">
+                <th>{{index}}</th>
+                <td>{{item[0]}}</td>
+                <td>{{item[1]}}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+
+
+        <div>
+          <h1 class="text-5xl font-bold">FFXIV Player Label</h1>
+          <p class="py-6">
+            ようこそ♥このプロジェクトはエゴイストのスタッフさんのために創設されました🎉<br>
+            自動でキャラクターを検知してキャラクターの情報を知らせてくれるウェブアプリです♥
+          </p>
+          <button @click="windowCapture()" class="btn btn-primary">スタート！<i class="mdi mdi-monitor"></i></button>
+          <a href="https://docs.google.com/spreadsheets/d/1QyVdvmeGgUs1sej-wVs8ec4g8XfnTLs2V6oHEzYcPcM/edit?usp=sharing" target="_blank" rel="noopener noreferrer">
+            <button class="btn btn-accent mx-3">キャラクターを登録 <i class="mdi mdi-checkbox-multiple-blank-outline"></i></button>
+          </a>
+          <label for="my-modal-4" class="btn btn-secondary">ko-fi♥</label>
+          <label for="my-modal-3" class="btn btn-secondary mx-3">paypay♥</label>
+        </div>
+      </div>
+    </div>
+
+
+
+    <input type="checkbox" id="my-modal-4" class="modal-toggle" />
+    <label for="my-modal-4" class="modal cursor-pointer">
+      <label class="modal-box relative" for="">
+        <iframe id='kofiframe' src='https://ko-fi.com/tsupata/?hidefeed=true&widget=true&embed=true&preview=true'
+          style='border:none;width:100%;padding:1px;background:#f9f9f9;' height='712' title='tsupata'></iframe>
+      </label>
+    </label>
+
+    <input type="checkbox" id="my-modal-3" class="modal-toggle" />
+    <label for="my-modal-3" class="modal cursor-pointer">
+      <label class="modal-box relative" for="">
+        <img src="./static/paypay.jpg">
+      </label>
+    </label>
+
+
+
+    <div class="stream-content">
+      <video ref="video" autoplay></video>
+      <canvas ref="canvas" width="1920" height="1080"></canvas>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.stream-content{
+  position: absolute;
+  top: 0;
+  right: 0;
+  z-index: 1000;
+}
+canvas,video{
+  max-width: 200px;
+}
+
+</style>
+
+<script>
+import { createWorker } from 'tesseract.js';
+
+export default {
+  data() {
+    return {
+      gs_players: [],
+      players: [],
+      worker: null,
+      toggle: {
+        firstpage:true
+      }
+    }
+  },
+  methods: {
+    detect() {
+      const rectangle = { left: 300, top: 80, width: 1400, height: 810 };
+
+      (async () => {
+        const { data: { text } } = await this.worker.recognize(this.$refs.canvas,rectangle);
+
+        let matchedTexts = text.match(/[A-Z]{1}[a-z] [A-Z]{1}[a-z]/g)
+        console.log("matchedTexts", matchedTexts)
+
+        if (matchedTexts == null) return
+
+        let l1 = []
+
+        let l2 = []
+
+        matchedTexts.forEach((x,i) => {
+          l1[i] = x.toLowerCase().replace(/[^a-z]/,'')
+        });
+        this.gs_players.forEach((x, i) => {
+          l2[i] = x[0].toLowerCase().replace(/[^a-z]/,'')
+        })
+
+        let l3 = []
+
+        l1.forEach((x, i) => {
+          l2.forEach((y, j) => {
+            if(x == y) l3.push(y)
+          })
+        })
+
+        if (l3.length) {
+
+          l3.forEach((x, i) => {
+            this.gs_players.forEach((y, j) => {
+              if(x == y[0]) this.players.unshift(y)
+            })
+          })
+          const x = new Set(this.players)
+          this.players = array([...x])
+        }
+      })()
+    },
+
+    windowCapture() {
+      (async () => {
+        this.$refs.video.srcObject = await navigator.mediaDevices.getDisplayMedia({ video: { cursor: "always" }, audio: false })
+      })()
+    }
+  },
+
+
+
+
+  mounted() {
+
+    fetch("https://script.google.com/macros/s/AKfycbwoHedneUlLGbqSZxU97ES2-0nkSIB8eTjDIeNqckesVsYnZ0QgXLK9CP1Ux52GWL0P/exec")
+      .then(response => response.json())
+      .then(data => this.gs_players = data)
+
+    this.worker = createWorker();
+    (async () => {
+      await this.worker.load();
+      await this.worker.loadLanguage('eng');
+      await this.worker.initialize('eng');
+      await this.worker.setParameters({
+        tessedit_char_whitelist: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz',
+      });
+      setInterval(() => {
+        this.$refs.canvas.getContext('2d').drawImage(this.$refs.video, 0, 0, 1920, 1080)
+        this.detect()
+      }, 13000);
+    })();
+
+
+  },
+  beforeDestroy() {
+    (async () => {
+      await this.worker.terminate();
+    })()
+  },
+}
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,26 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  modules: [
+    // Docs: https://tailwindcss.nuxtjs.org
+    "@nuxtjs/tailwindcss",
+  ],
+  head: {
+    title: 'FFXIV Player Label',
+    link: [{ rel: "icon", type: "image/x-icon", href: "./static/logos/favion.ico" }]
+  },
+  app: {
+    head: {
+      htmlAttrs: {
+        "data-theme": "light",
+      },
+    },
+  },
+  vite: {
+    define: {
+      "process.env.TESS_ENV": process.env.ENV,
+    },
+  },
+  css: [
+    '@mdi/font/css/materialdesignicons.css'
+  ]
+})
