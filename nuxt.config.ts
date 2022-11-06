0af85b9850c34930a2e5228a8d4e4d710851eca0
CYB0RG <i.love.hard.player@gmail.com>
2022-11-07 05:47:28 +0900

    image PNG to webp

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,19 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
+
 export default defineNuxtConfig({
   modules: [
     // Docs: https://tailwindcss.nuxtjs.org
     "@nuxtjs/tailwindcss",
   ],
-  head: {
-    title: 'FFXIV Player Label',
-    link: [{ rel: "icon", type: "image/x-icon", href: "./static/logos/favion.ico" }]
-  },
+
   app: {
     head: {
       htmlAttrs: {
         "data-theme": "dark",
       },
+      viewport: 'width=500, initial-scale=1',
+      title: 'FFXIV Player Label',
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
     },
   },
   vite: {
